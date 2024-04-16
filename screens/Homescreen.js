13cfe23ede30973d66246ab35224f232e2533426
Harryuen <141343677+Harryuen@users.noreply.github.com>
2024-04-15 22:49:41 -0600

    Update Homescreen.js

--- screens/Homescreen.js
+++ screens/Homescreen.js
@@ -1,15 +1,28 @@
+//Homescreen.js
 import React from "react";
-import { SafeAreaView, StyleSheet } from "react-native";
+import { SafeAreaView, StyleSheet, Text, View, Image } from "react-native";
 import TitleHeader from "../components/TitleHeader";
 import { useColors } from "../components/useColors";
 
-const Homescreen = ({ navigation }) => {
-  const { background } = useColors();
+const Homescreen = ({ navigation, isDarkMode }) => {
+  const { background, text } = useColors(isDarkMode);
 
   return (
     <SafeAreaView style={[styles.container, { backgroundColor: background }]}>
       <TitleHeader navigation={navigation}>Home</TitleHeader>
-      {/* TODO: STYLE AS A DASHBOARD AS A BRIEF OVERVIEW OF EVERYTHING */}
+      <View style={styles.imageContainer}>
+        <Image
+          source={require("../assets/Quiz.png")}
+          style={styles.image}
+          resizeMode="contain"
+        />
+        <Image
+          source={require("../assets/Task.png")}
+          style={styles.image}
+          resizeMode="contain"
+        />
+      </View>
+      <Text style={{ color: text }}>Testing color</Text>
     </SafeAreaView>
   );
 };
@@ -19,6 +32,15 @@ const styles = StyleSheet.create({
     flex: 1,
     // alignItems: "center",
     // justifyContent: "center",
+  },
+  imageContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingHorizontal: 16,
+  },
+  image: {
+    height: 200, // Adjust the height as needed
+    width: "45%", // Adjust the width as needed
   },
 });
 
