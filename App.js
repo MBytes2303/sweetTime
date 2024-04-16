1d955da0cd5f960f1bd271249318451e10358cab
John Matthew Bayona <matthewbayona01@gmail.com>
2024-04-15 23:49:51 -0600

    removed homescreen

--- App.js
+++ App.js
@@ -5,16 +5,14 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
 import { MaterialIcons } from "@expo/vector-icons";
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 import { TaskProvider } from "./TaskContext";
 
 import Loginscreen from "./screens/Loginscreen";
-import Homescreen from "./screens/Homescreen";
 import Taskscreen from "./screens/Taskscreen";
 import Calendarscreen from "./screens/Calendarscreen";
 import Settingscreen from "./screens/Settingscreen";
-
 
 const Tab = createMaterialBottomTabNavigator();
 const Stack = createNativeStackNavigator();
@@ -27,16 +25,6 @@ function TabNavigator() {
       inactiveColor="#979290"
       barStyle={{ backgroundColor: "#FFBE98" }}
     >
-      <Tab.Screen
-        name="Home"
-        component={Homescreen}
-        options={{
-          tabBarLabel: "Home",
-          tabBarIcon: ({ color }) => (
-            <MaterialIcons name="home" color={color} size={26} />
-          ),
-        }}
-      />
       <Tab.Screen
         name="Task"
         component={Taskscreen}
@@ -80,45 +68,44 @@ export default function App() {
   return (
     <TaskProvider>
       <NavigationContainer>
-      <Stack.Navigator>
-  {!isLoggedIn ? (
-    <Stack.Screen
-      name="Login"
-      options={{ title: "Login" }}
-    >
-      {props => <Loginscreen {...props} handleLogin={handleLogin} />}
-    </Stack.Screen>
-  ) : (
-    <>
-      <Stack.Screen
-        name="Main"
-        component={TabNavigator}
-        options={{ headerShown: false }}
-      />
-      <Stack.Screen
-  name="Setting"
-  options={{
-    headerStyle: {
-      backgroundColor: "#FFBE98",
-    },
-    headerTitleStyle: {
-      fontSize: 32,
-      fontWeight: "bold",
-    },
-    headerRight: () => (
-      <Button
-        onPress={() => handleLogout(setIsLoggedIn)}
-        title="Logout"
-        color="#FF0000"
-      />
-    ),
-  }}
->
-  {props => <Settingscreen {...props} setIsLoggedIn={setIsLoggedIn} />}
-</Stack.Screen>
-    </>
-  )}
-</Stack.Navigator>
+        <Stack.Navigator>
+          {!isLoggedIn ? (
+            <Stack.Screen name="Login" options={{ title: "Login" }}>
+              {(props) => <Loginscreen {...props} handleLogin={handleLogin} />}
+            </Stack.Screen>
+          ) : (
+            <>
+              <Stack.Screen
+                name="Main"
+                component={TabNavigator}
+                options={{ headerShown: false }}
+              />
+              <Stack.Screen
+                name="Setting"
+                options={{
+                  headerStyle: {
+                    backgroundColor: "#FFBE98",
+                  },
+                  headerTitleStyle: {
+                    fontSize: 32,
+                    fontWeight: "bold",
+                  },
+                  headerRight: () => (
+                    <Button
+                      onPress={() => handleLogout(setIsLoggedIn)}
+                      title="Logout"
+                      color="#FF0000"
+                    />
+                  ),
+                }}
+              >
+                {(props) => (
+                  <Settingscreen {...props} setIsLoggedIn={setIsLoggedIn} />
+                )}
+              </Stack.Screen>
+            </>
+          )}
+        </Stack.Navigator>
       </NavigationContainer>
     </TaskProvider>
   );
